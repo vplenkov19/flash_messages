46b4bf5dc304f0718165a37248d5f2bbf9069018
ViskoPl <visko@arsfutura.co>
2020-03-30 13:31:48 +0200

    remove modal javascript because we are using boostrap

--- app/assets/javascripts/global/helpers.js
+++ app/assets/javascripts/global/helpers.js
@@ -1,19 +1,4 @@
 __App = {};
 
 __App._Helpers = {
-	getAppendModal: function ( path ) {
-		return $.ajax( {
-			url: path,
-			method: "GET",
-			dataType: "json"
-		} );
-	},
-	removeModal: function () {
-		$( '[data-item="full-overlay"]' ).empty();
-		$( '[data-item="full-overlay"]' ).removeClass( 'overlay--active' );
-	},
-	appendModal: function ( data ) {
-		$( '[data-item="full-overlay"]' ).addClass( 'overlay--active' );
-		$( '[data-item="full-overlay"]' ).html( data.html_content );
-	}
 };
